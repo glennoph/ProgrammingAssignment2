b201dc70e5c5c84a001dbb708661d7570bf71198
glenn opdycke-hansen <glennoph@gmail.com>
2015-05-21 19:22:20 -0500

    add logic and some comments to the R code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## provide a cache of the matrix and the solution of a system of equations
+## makeCacheMatrix holds the matrix x and the solution, inv
+## cacheSolve will either return the cached solution or will solve and cache the inverse
 
-## Write a short comment describing this function
+## make cache of matrix and the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv = NULL 
+    # set is a function that sets matrix x and nulls the inverse (inv)
+    # note that x and inv reside in the parent 
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    # get is a function that returns matrix x
+    get <- function() { x }
+    # setinv with a parameter, solve sets the inv in the parent
+    setinv <- function( solve ) { inv <<- solve }
+    # setinv with no parameters
+    setinv <- function() { inv }
+    list( set=set, get=get, setinv=setinv, getinv=getinv )
 }
 
-
-## Write a short comment describing this function
+## cacheSolve is a fuction that returns either the cached inverse (inv) 
+## or solves the matrix, caches the inverse, and returns the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    # if the cached inverse (inv) is not null then write a message and return inv
+    if (!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    # if code gets here then the inverse is solved and cached for later
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
